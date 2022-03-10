a75d7516622242d3c8c222c71dc35376b17f55c1
Vonchyy <VoncheyK@gmail.com>
2022-03-10 22:07:40 +0200

    amo

---
+++ source/ClientSettings.hx
@@ -0,0 +1,10 @@
+package;
+
+import flixel.FlxG;
+
+class ClientSettings
+{
+    public static var downScroll:Bool = FlxG.save.data.downScroll;
+    public static var ghostTapping:Bool = FlxG.save.data.ghostTapping;
+    public static var noteskin:String = FlxG.save.data.noteskin;
+}
